ea8ec9133a61526878f102b0e87e6b68b85a1c59
Fernando Gallo <fernandgallo@gmail.com>
2023-06-03 18:20:22 -0300

    Move HTTPClient to its own file

---
+++ AleloStore/AleloStore/Products API/HTTPClient.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Fernando Gallo on 03/06/23.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
