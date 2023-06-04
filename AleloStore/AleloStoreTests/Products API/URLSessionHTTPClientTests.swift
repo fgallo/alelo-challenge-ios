b9f9955f2c1c39d5ab7d000afc00b27d65a7d0c8
Fernando Gallo <fernandgallo@gmail.com>
2023-06-04 12:10:54 -0300

    Delivers unexpected	values error when request completes with data, response and error as nil

--- AleloStore/AleloStoreTests/Products API/URLSessionHTTPClientTests.swift
+++ AleloStore/AleloStoreTests/Products API/URLSessionHTTPClientTests.swift
@@ -12,10 +12,14 @@ class URLSessionHTTPClient {
         self.session = session
     }
     
+    struct UnexpectedValuesRepresentation: Error {}
+    
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
         }.resume()
     }
@@ -54,11 +58,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 0)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
-        let sut = makeSUT()
-        
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: anyURL()) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -73,7 +75,24 @@ class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
-    
+    func test_getFromURL_failsOnAllNilValues() {
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+        
+        let exp = expectation(description: "Wait for completion")
+        
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Expected failure, got \(result) instead.")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
     
     // MARK: - Helpers
     
