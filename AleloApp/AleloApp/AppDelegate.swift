97bfcbab24161e94ffe6d9c6b0b803997016ef38
Fernando Gallo <fernandgallo@gmail.com>
2023-06-08 16:21:56 -0300

    Add empty AleloApp project

---
+++ AleloApp/AleloApp/AppDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Fernando Gallo on 08/06/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ AleloApp/AleloAppTests/AleloAppTests.swift
@@ -0,0 +1,8 @@
+//
+//  Created by Fernando Gallo on 08/06/23.
+//
+
+import XCTest
+@testable import AleloApp
+
+final class AleloAppTests: XCTestCase {}
