ecf0aa3c0f4fb3c58858b572b7594cb8f6c7f728
Fernando Gallo <fernandgallo@gmail.com>
2023-06-08 10:46:03 -0300

    Extract duplicate retrieve test code into a reusable helper method

--- AleloStore/AleloStoreTests/Cart Cache/CodableCartStoreTests.swift
+++ AleloStore/AleloStoreTests/Cart Cache/CodableCartStoreTests.swift
@@ -46,85 +46,42 @@ class CodableCartStoreTests: XCTestCase {
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
         
-        sut.retrieve { result in
-            switch result {
-            case .empty:
-                break
-            default:
-                XCTFail("Expected empty result, got \(result) instead")
-            }
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieve: .empty)
     }
     
     func test_retrieveTwice_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
         
-        sut.retrieve { firstResult in
-            sut.retrieve { secondResult in
-                switch (firstResult, secondResult) {
-                case (.empty, .empty):
-                    break
-                default:
-                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
-                }
-                exp.fulfill()
-            }
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieveTwice: .empty)
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
         let sut = makeSUT()
         let cart = makeCart().local
-        let exp = expectation(description: "Wait for cache retrieval")
-
+        
+        let exp = expectation(description: "Wait for cache insertion")
         sut.insert(cart) { insertionError in
             XCTAssertNil(insertionError, "Expected cart to be inserted successfully")
-            
-            sut.retrieve { retrieveResult in
-                switch retrieveResult {
-                case let .found(retrievedCart):
-                    XCTAssertEqual(retrievedCart, cart)
-                default:
-                    XCTFail("Expected found result with cart \(cart), got \(retrieveResult) instead")
-                }
-                exp.fulfill()
-            }
+            exp.fulfill()
         }
-
         wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toRetrieve: .found(cart))
     }
     
     func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
         let sut = makeSUT()
         let cart = makeCart().local
-        let exp = expectation(description: "Wait for cache retrieval")
-
+        
+        let exp = expectation(description: "Wait for cache insertion")
         sut.insert(cart) { insertionError in
             XCTAssertNil(insertionError, "Expected cart to be inserted successfully")
-            
-            sut.retrieve { firstResult in
-                sut.retrieve { secondResult in
-                    switch (firstResult, secondResult) {
-                    case let (.found(firstRetrievedCart), .found(secondRetrievedCart)):
-                        XCTAssertEqual(firstRetrievedCart, cart)
-                        XCTAssertEqual(secondRetrievedCart, cart)
-                    default:
-                        XCTFail("Expected retrieving twice from non empty cache to deliver same found result with cart \(cart), got \(firstResult) and \(secondResult) instead")
-                    }
-                    exp.fulfill()
-                }
-            }
+            exp.fulfill()
         }
-
         wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toRetrieveTwice: .found(cart))
     }
     
     // MARK: - Helpers
@@ -133,6 +90,31 @@ class CodableCartStoreTests: XCTestCase {
         let sut = CodableCartStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: CodableCartStore, toRetrieveTwice expectedResult: RetrieveCachedCartResult, file: StaticString = #filePath, line: UInt = #line) {
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+    }
+    
+    private func expect(_ sut: CodableCartStore, toRetrieve expectedResult: RetrieveCachedCartResult, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { retrievedResult in
+            switch (expectedResult, retrievedResult) {
+            case (.empty, .empty):
+                break
+                
+            case let (.found(expectedCart), .found(retrievedCart)):
+                XCTAssertEqual(retrievedCart, expectedCart, file: file, line: line)
+                
+            default:
+                XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func testSpecificStoreURL() -> URL {
