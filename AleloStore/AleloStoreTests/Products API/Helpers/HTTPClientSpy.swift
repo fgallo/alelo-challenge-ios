2ec8b693a2956c202769de8c76b4a08561eb355d
Fernando Gallo <fernandgallo@gmail.com>
2023-06-08 15:19:51 -0300

    Refactor to make `HTTPClient` completes with `Swift.Result` type

--- AleloStore/AleloStoreTests/Products API/Helpers/HTTPClientSpy.swift
+++ AleloStore/AleloStoreTests/Products API/Helpers/HTTPClientSpy.swift
@@ -6,13 +6,13 @@ import Foundation
 import AleloStore
 
 class HTTPClientSpy: HTTPClient {
-    private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+    private var messages = [(url: URL, completion: (HTTPClient.Result) -> Void)]()
     
     var requestedURLs: [URL] {
         messages.map { $0.url }
     }
     
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         messages.append((url, completion))
     }
     
@@ -28,6 +28,6 @@ class HTTPClientSpy: HTTPClient {
             headerFields: nil
         )!
         
-        messages[index].completion(.success(data, response))
+        messages[index].completion(.success((data, response)))
     }
 }
